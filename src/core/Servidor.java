82c569c380b70bf93827b6ce3a50ca3fab3b8f8c
unknown <Aluno@SSA-D-LINF05-11.intranet.cefetba.br>
2023-08-22 20:14:54 -0300

    Remove unasebled imports

--- src/core/Servidor.java
+++ src/core/Servidor.java
@@ -1,28 +1,23 @@
 package core;
-import java.io.BufferedReader;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectOutputStream;
-import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Servidor {
 	private ServerSocket serverSocket;
-	private Socket socket;
+	private Socket conexao;
 	private Map<String, ObjectOutputStream> mapOnlines = new HashMap<String, ObjectOutputStream>();
 	
 	public Servidor() {
 	    try {
-	        ServerSocket server = new ServerSocket(5555);
+	    	serverSocket = new ServerSocket(5555);
 	        System.out.println("ServidorSocket rodando na porta 5555");
 	        while (true) {
-	            Socket conexao = server.accept();
+	             conexao = serverSocket.accept();
 	        }
 	    } catch (IOException e) {
 	
